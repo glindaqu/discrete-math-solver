32af49aa7ce0b3c00f22cd9b1e4bab84713d2933
Vladislav <q5481996@gmail.com>
2022-10-29 16:43:05 +0700

    fixed input pattern and added small changes into design

--- script.js
+++ script.js
@@ -4,16 +4,9 @@ const getRandomElement = (arr, len) => arr[Math.floor(Math.random() * len)];
 const getInversion = () => Math.random()*2 > 1 ? '!' : ``;
 const isExpression = () => Math.random()*2 > 1 ? true : false;
 
-$('.container button').on('click', () => {
-    let dificult = Number($('.dificult').val());
-    let variables = $('.variables').val().split(', ');
-    console.log(variables);
-    $('.res__data').text(main(dificult, variables));
-});
 
 const main = (dificult, variables) => {
     let res = '';
-
     for (i=0; i<dificult; i++) {
         if (isExpression()) {
             res += `(${getInversion() + getRandomElement(variables, variables.length)}`;
@@ -31,6 +24,15 @@ const main = (dificult, variables) => {
             res += ' ' + getRandomElement(operations, operations.length) + ' ';
         }
     }
-    
     return res;
-}
+};
+
+$('.container button').on('click', () => {
+    let dificult = Number($('.dificult').val());
+    let variables = $('.variables').val().replaceAll(" ", "").split(",");
+    console.log(variables);
+    $('.res__data').text(main(dificult, variables));
+});
+
+
+
