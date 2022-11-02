4dcc6b44da45b3315cdfd1b8fdbb67da4c5a3f90
Vladislav <q5481996@gmail.com>
2022-11-02 20:31:32 +0700

    added new color theme and small animation

--- js/core.js
+++ js/core.js
@@ -1,26 +1,26 @@
 let operations = ['∧', 'v', '=>', '<=>'];
 
 const getRandomElement = (arr, len) => arr[Math.floor(Math.random() * len)];
-const getInversion = () => Math.random()*2 > 1 ? '!' : ``;
-const isExpression = () => Math.random()*2 > 1 ? true : false;
+const getInversion = () => Math.random() * 2 > 1 ? '!' : ``;
+const isExpression = () => Math.random() * 2 > 1;
 
 
 const main = (dificult, variables) => {
     let res = '';
-    for (i=0; i<dificult; i++) {
+    for (i = 0; i < dificult; i++) {
         if (isExpression()) {
             res += `(${getInversion() + getRandomElement(variables, variables.length)}`;
-            res += ` ${getRandomElement(operations, operations.length)} `; 
+            res += ` ${getRandomElement(operations, operations.length)} `;
             res += `${getInversion() + getRandomElement(variables, variables.length)})`;
         } else {
             res += getInversion() + getRandomElement(variables, variables.length);
         }
-    
-        if (i>0 && i<dificult-1 && isExpression()) {
+
+        if (i > 0 && i < dificult - 1 && isExpression()) {
             res = `(${res})`;
         }
-    
-        if (i != dificult -1) {
+
+        if (i !== dificult - 1) {
             res += ' ' + getRandomElement(operations, operations.length) + ' ';
         }
     }
@@ -32,6 +32,4 @@ $('.container button').on('click', () => {
     let variables = $('.variables').val().replaceAll(" ", "").split(",");
     $('.res__data').text(main(dificult, variables));
 });
-
-
 
---
+++ js/script.js
@@ -0,0 +1,53 @@
+let change = false;
+
+if (localStorage.getItem("bg-color")) {
+    $('body').css('background-color', localStorage.getItem("bg-color"));
+    $('body, .variables, .dificult, .subtitle, .calc').css('color', localStorage.getItem("text-color"));
+    change = localStorage.getItem("isChange");
+}
+
+function playScale(selector) {
+    anime({
+        targets: `.${selector}`,
+        scale: 1.3,
+        duration: 350,
+        scaleX: [1, 0.8, 1],
+        easing: "easeOutCubic",
+        loop: false,
+        complete: () => {
+            anime({
+                targets: `.${selector}`,
+                scale: 1,
+                scaleY: [1, 0.8, 1],
+                duration: 350,
+                easing: "easeOutCubic",
+                loop: false
+            });
+        }
+    });
+}
+
+function switchTheme() {
+    change = !change;
+    if (change) {
+        localStorage.setItem("bg-color", "#3812aa98");
+        localStorage.setItem("text-color", "rgb(255,255,170)");
+    }
+    else {
+        localStorage.setItem("bg-color", "#e1d9ce");
+        localStorage.setItem("text-color", "black");
+    }
+    localStorage.setItem("isChange", change);
+    $('body').css('background-color', localStorage.getItem("bg-color"));
+    $('body, .variables, .dificult, .subtitle, .calc').css('color', localStorage.getItem("text-color"));
+    $('body, .variables, .dificult, .subtitle, .calc').css('transition', '1.5s ease');
+    //$('.calc').css('background-color', change ? "" : "rgba(79,69,103, .55)");
+
+
+}
+
+$('.owl').on('click', () => {
+    playScale("owl");
+    switchTheme();
+});
+
